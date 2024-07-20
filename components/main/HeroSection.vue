98ffad3a8fc46c25d53ffb78ee2fb0cece448040
Rustam-Fozilov <fozilovrustam32@gmail.com>
2024-07-20 18:17:47 +0500

    update: hero section

--- components/main/HeroSection.vue
+++ components/main/HeroSection.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full h-screen lg:pt-32 lg:h-auto lg:pb-12">
+  <div class="w-full bg-soft-tiffany h-screen lg:pt-32 lg:h-auto lg:pb-12">
     <div class="my-container h-full flex justify-between items-center lg:flex-col lg:justify-around lg:gap-7">
       <div class="w-1/2 lg:w-full lg:flex lg:flex-col lg:items-center lg:text-center lg:order-2">
         <div id="hero-title" class="text-4xl text-dark-green leading-none tracking-tight sm:text-3xl">
@@ -14,9 +14,9 @@
           </a>
         </div>
       </div>
-      <div id="hero-image_container" class="animate-in slide-in-from-bottom fade-in-0 duration-1000 h-auto lg:order-1 lg:w-auto lg:h-[55%]">
+      <div id="hero-image_container" class="mt-12 animate-in slide-in-from-bottom fade-in-0 duration-1000 h-auto lg:order-1 lg:w-auto lg:h-[55%] lg:mt-0">
         <div class="w-auto h-auto lg:h-full lg:w-full">
-          <img id="hero-image" class="w-full h-full object-cover lg:object-contain" src="/assets/images/main/hero.png" alt="Hero image">
+          <img id="hero-image" class="w-full h-full object-cover lg:object-contain" src="/assets/images/main/hero-merge.png" alt="Hero image">
         </div>
       </div>
     </div>
